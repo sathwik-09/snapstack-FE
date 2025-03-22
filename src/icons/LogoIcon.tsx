b64c1d238e98426486a3288da66f61a3204b3fd1
Sathwik <enapothulasathwik09@gmail.com>
2025-03-22 20:41:32 +0530

    UI library Icons

---
+++ src/icons/LogoIcon.tsx
@@ -0,0 +1,10 @@
+export const LogoIcon = ()=>{
+  return <svg xmlns="http://www.w3.org/2000/svg" stroke="currentColor" width="50" height="50" viewBox="0 0 50 50" fill="#5046E4">
+  <rect x="4" y="4" width="42" height="42" rx="5" stroke="#5046E4" stroke-width="2" fill="none"/>
+  <path d="M15 10 H35 V38 L25 30 L15 38 Z" fill="#5046E4"/>
+  <line x1="18" y1="15" x2="32" y2="15" stroke="#5046E4" stroke-width="2"/>
+  <line x1="18" y1="20" x2="32" y2="20" stroke="#5046E4" stroke-width="2"/>
+</svg>
+
+
+}
