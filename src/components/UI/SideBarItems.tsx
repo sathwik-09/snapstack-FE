483745ba500cb2f7ff7841c4e8a834bfce838ae5
Sathwik <enapothulasathwik09@gmail.com>
2025-03-28 21:38:42 +0530

    refact UI

--- src/components/UI/SideBarItems.tsx
+++ src/components/UI/SideBarItems.tsx
@@ -5,13 +5,13 @@ interface SideBarItemProps {
   icon: ReactElement;
 }
 
-export const SideBarItems = ({text, icon}: SideBarItemProps) => {
-  return <div className="flex text-gray-700 py-2 cursor-pointer hover:bg-gray-200 rounded max-w-48 pl-4 transition-all duration-700">
-    <div className="pr-2 pb-2 ">
-      {icon}
+export const SideBarItems = ({ text, icon }: SideBarItemProps) => {
+  return (
+    <div className="flex items-center text-gray-700 py-3 px-4 cursor-pointer hover:bg-indigo-500 hover:text-white rounded-lg transition-all duration-300 transform hover:scale-105">
+      <div className="mr-4 p-2 rounded-full bg-indigo-100 text-indigo-600 flex items-center justify-center">
+        {icon}
+      </div>
+      <div className="text-lg font-medium">{text}</div>
     </div>
-    <div className="cursor-pointer">
-      {text}
-    </div> 
-  </div>
-}
+  );
+};
